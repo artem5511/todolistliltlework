cc8fe775e6152c6542c9c392ba83fcdfa7aaf6b0
artem_ <artem_5511@mail.ru>
2023-03-29 23:22:47 +0300

    one

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//function declaration
 function App() {
+    //полезное что то
+
+    //обязана вернуть JSX
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     This is APP component
+        <Rating/>
+        <Accordion/>
     </div>
   );
+}
+
+function hello() {
+    debugger
+    alert("Hello")
+}
+
+function Rating() {
+    return (
+        <div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+        </div>
+    );
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Меню</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
